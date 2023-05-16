e6b10dab1b01a07f00363653a4b5c18c88c79059
annabellefosburgh <afosburgh20@gmail.com>
2023-05-16 09:31:34 -0500

    created routes folder and .js file

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const app = express();
 app.listen(3001)
 
 //Initializing parse info
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 //A function used to write a new note
+function writeNote()
 
 //A function used to delete a note ((BONUS, FINISH LAST))
 
