e0569be210abf4e4398ca98d78a12376a330c707
annabellefosburgh <afosburgh20@gmail.com>
2023-08-05 15:51:02 -0500

    Debugging, moving api requests, deleting delete request

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+require('./api/notes')
 const { v4: uuidv4 } = require('uuid');
-
 
 //Initializing express
 const app = express();
@@ -14,7 +14,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//Get request for all saved notes
+
+//Gets the home page and returns our index html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+//Grabs all notes and returns our notes html
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// app.get("/assets/css/styles.css", (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/assets/css/styles.css'))
+// })
+
+
+//Grabs our index html for anything else
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
+
+
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
@@ -23,39 +44,35 @@ app.get('/api/notes', (req, res) => {
     });   
 })
 
-//Post request for a new note to add to the rest of the notes
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body
-    newNote.id = uuidv4()
-    db.push(newNote)
-    fs.writeFileSync('./db/db.json', JSON.stringify(db))
-    res.json(db)
+    const {title, text} = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        }
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+                parsedNotes.push(newNote)
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (err) => {
+                    err ? console.log(err) : console.log('successfully added note')
+                })
+            }
+        })
+        const response = {
+            status: 'success',
+            body: newNote
+        }
+        console.log(response);
+        res.json(response);
+    } else {
+        res.json('unable to post note')
+    }
 })
-
-//Deletes a note by using it's specific id
-app.delete('/api/notes/:id', (req, res) => {
-    const newDb = db.filter((note) =>
-        note.id !== req.params.id)
-    fs.writeFileSync('./db/db.json', JSON.stringify(newDb))
-    readFile.json(newDb)
-})
-
-//Gets the home page and returns our index html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-
-//Grabs all notes and returns our notes html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
-})
-
-//Grabs our index html for anything else
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-
-
 
 app.listen(PORT, () => {
     console.log("Express web server running on port " + PORT);
